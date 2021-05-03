a28c430198e5ed36f6ed7d33b9066e303376fcd1
SYADAV81 <mail2sharadyadav@gmail.com>
2021-05-03 17:54:59 +0530

    modified

---
+++ OSMS.Core/Services/Student/StudentManager.cs
@@ -0,0 +1,14 @@
+﻿using OSMS.Infrastructure;
+using OSMS.Infrastructure.DataRepository;
+
+namespace OSMS.Domain.Services.Student
+{
+    public class StudentManager : EFGenericDataRepository<OSMS.Infrastructure.DataEntities.Student>, IStudentManager
+    {
+        public StudentManager(ApplicationDbContext context) : base(context)
+        {
+
+        }
+
+    }
+}
